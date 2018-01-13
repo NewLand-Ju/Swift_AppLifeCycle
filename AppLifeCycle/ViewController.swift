1b21ec4380764001e30458f620104f0c9d0acf76
JuDH <forever43591@gmail.com>
2018-01-14 04:21:32 +0900

    Initial commit

--- AppLifeCycle/ViewController.swift
+++ AppLifeCycle/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    override func loadView() {
+//        super.loadView()
+//        print("loadView")
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
     }
 
     override func didReceiveMemoryWarning() {
